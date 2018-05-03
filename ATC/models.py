1ec919c0727170434318f541ceb7fe0b0a7fc679
Rohit Khanwani <rohit@dhcp-rhodes-2878.eduroam.cornell.edu>
2018-05-03 18:17:50 -0400

    Removed indexes from properties

--- ATC/models.py
+++ ATC/models.py
@@ -1,13 +1,15 @@
 from google.appengine.ext import ndb
 
+# SET INDEXED TO FALSE FOR FASTER WRITES
+
 # Stores the current position of the flight
 class Flight(ndb.Model):
     # key = ndb.KeyProperty()
-    flight_num = ndb.StringProperty()
-    altitude = ndb.FloatProperty()
-    speed = ndb.FloatProperty()
-    location = ndb.GeoPtProperty()
-    temperature = ndb.FloatProperty()
+    flight_num = ndb.StringProperty(indexed=False)
+    altitude = ndb.FloatProperty(indexed=False)
+    speed = ndb.FloatProperty(indexed=False)
+    location = ndb.GeoPtProperty(indexed=False)
+    temperature = ndb.FloatProperty(indexed=False)
 
 class Route(ndb.Model):
     waypoints = ndb.GeoPtProperty(repeated=True)
@@ -18,13 +20,13 @@ class FlightPlan(ndb.Model):
     #flight number should be a mixture of carrier code and number - will be used as key
     #to get all other attributes of the flight
     flight_num = ndb.StringProperty()
-    origin = ndb.StringProperty()
-    dest = ndb.StringProperty()
+    origin = ndb.StringProperty(indexed=False)
+    dest = ndb.StringProperty(indexed=False)
     #can combine date and time to make date time property for dep and arr
-    dep_time = ndb.DateTimeProperty()
-    arr_time = ndb.DateTimeProperty()
-    cancelled = ndb.BooleanProperty()
-    carrier = ndb.StringProperty()
+    dep_time = ndb.DateTimeProperty(indexed=False)
+    arr_time = ndb.DateTimeProperty(indexed=False)
+    cancelled = ndb.BooleanProperty(indexed=False)
+    carrier = ndb.StringProperty(indexed=False)
     current_route = ndb.StructuredProperty(Route)
 
 # Stores the next waypoint for the flight to go to
@@ -37,7 +39,7 @@ class FlightWaypoints(ndb.Model):
     flight_plan_urlsafe = ndb.StringProperty()
     flight_urlsafe = ndb.StringProperty()
     current_route_index = ndb.IntegerProperty()
-    last_updated = ndb.DateTimeProperty()
+    last_updated = ndb.DateTimeProperty(auto_now=True)
 
 class Routes(ndb.Model):
     origin = ndb.StringProperty()
