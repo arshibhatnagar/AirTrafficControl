fe549987d81699002cd987185cd687d7af111e41
Rohit Khanwani <rohit@rohits-mbp.home>
2018-05-08 22:39:18 -0400

    No need for cron job anymore

--- ATC/live_data.py
+++ ATC/live_data.py
@@ -62,7 +62,8 @@ if __name__ == "__main__":
     num_threads = int(sys.argv[1])
     file = open(DATA_FILE)
     data_list = json.load(file)
-    threadpool = [Sensor(i, data_list[i], np.random.randint(0, 10)) for i in range(num_threads)]
+    threadpool = [Sensor(i, data_list[i], np.random.randint(0, 50)) for i in range(num_threads)]
+    requests.get("http://localhost:8081/waypoint_updates")
     for thread in threadpool:
         thread.start()
 
