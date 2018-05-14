ea7342b5eef0fe14c5f909048c41d706bd8647fb
Rohit Khanwani <rohit@dhcp-hol-3262.redrover.cornell.edu>
2018-05-13 20:57:31 -0400

    Added collision detection

--- ATC/tier2.py
+++ ATC/tier2.py
@@ -14,53 +14,71 @@ client = memcache.Client()
 @ndb.synctasklet
 def execute_updates():
     waypoints_qry = FlightWaypoints.query().order(FlightWaypoints.version)
-    # Can use the map function for tasklets. Look into that.
     results = yield waypoints_qry.fetch_async()
-    # last_page = False
-    # if not more:
-    #     last_page = True
-    # while (more or last_page):
-    #     if (last_page):
-    #         last_page = False
-    #     if more:
-    #         next_results, next_cursor, next_more = yield waypoints_qry.fetch_page_async(page_size=10, start_cursor=cursor)
-    #         if (not next_more):
-    #             last_page = True
-
     flight_plan_keys = [ndb.Key(urlsafe=flight.flight_plan_urlsafe) for flight in results]
     flight_keys = [ndb.Key(urlsafe=flight.flight_urlsafe) for flight in results]
     flight_plans, flights = yield ndb.get_multi_async(flight_plan_keys), ndb.get_multi_async(flight_keys)
     new_flight_parameters = [flight.location for flight in flights]
 
     to_cache = {}
+    flights_to_waypoints = {}
+    flights_to_altitudes = {}
 
     for i in range(len(results)):
         flight_waypoints = results[i]
         flight = flights[i]
         flight_plan = flight_plans[i]
 
-        new_parameters = {'next_waypoint': flight_waypoints.next_waypoint, 'next_altitude': 5000.0, 'next_speed': 975.0}
-        if ((flight.location.lat- flight_waypoints.next_waypoint.lat)**2 + (flight.location.lon - flight_waypoints.next_waypoint.lon)**2)**0.5 < 0.1:
+        new_parameters = {'next_waypoint': flight_waypoints.next_waypoint, 'next_altitude': 20000.0, 'next_speed': 975.0}
+        if ((flight.location.lat- flight_waypoints.next_waypoint.lat)**2 + (flight.location.lon - flight_waypoints.next_waypoint.lon)**2)**0.5 < 0.08:
             if (flight_waypoints.current_route_index < len(flight_plan.current_route.waypoints) - 1):
                 new_parameters['next_waypoint'] = flight_plan.current_route.waypoints[flight_waypoints.current_route_index + 1]
                 flight_waypoints.current_route_index += 1
+
+        
+
+        next_wp = new_parameters['next_waypoint']
+        flight_num = flight.flight_num
+
+        if flight_num in flights_to_altitudes:
+            #print("HALA MADRID")
+            new_parameters['next_altitude'] = flights_to_altitudes[flight_num]
+
+        max_alt = 0
+        flightlist = flights_to_waypoints.keys()
+        for flight_id in flightlist:
+            flight_wp = flights_to_waypoints[flight_id]
+            if flight_id != flight_num:
+                if ((next_wp.lat - flight_wp.lat)**2 + (next_wp.lon - flight_wp.lon)**2)**0.5 < 0.03:
+                    if abs(new_parameters['next_altitude'] - flights_to_altitudes[flight_id]) < 300:
+                        print("Potential collision between " + str(flight_num) + " and " + str(flight_id) + " detected. Altitude adjustment initialized.")
+                        print("Altitudes are: " + str(new_parameters['next_altitude']) + ", " +  str(flights_to_altitudes[flight_id]))
+                        max_alt = max(max_alt, flights_to_altitudes[flight_id])
+                        #print ('max_alt is: ' + str(max_alt))
+                    
+        if max_alt > 0:
+            new_parameters["next_altitude"] = max_alt + 350
+        else:
+            a = 1
+            #new_parameters['next_altitude'] = 20000.0
+        # if new_parameters['next_altitude'] > 30000.0:
+        #     a = 1
+        #     print "High altitude " + str(new_parameters['next_altitude'])
+        flights_to_altitudes[flight_num] = new_parameters["next_altitude"]
+        flights_to_waypoints[flight_num] = next_wp
+        #print('altitude for flight ' + str(flight_num) + ' is: ' + str(new_parameters['next_altitude']))
 
         flight_waypoints.next_waypoint = new_parameters['next_waypoint']
         flight_waypoints.next_altitude = new_parameters['next_altitude']
         flight_waypoints.next_speed = new_parameters['next_speed']
         flight_waypoints.version = flight.version
-
+        
+        print(flights_to_altitudes)
+        
         client.set(flight_waypoints.flight_urlsafe, flight_waypoints)
 
-            # to_cache[flight_waypoints.flight_urlsafe] = flight_waypoints
-
     yield ndb.put_multi_async(results)
-        # print client.set_multi(to_cache)
-
-        # if more:
-        #     results = next_results
-        #     cursor = next_cursor
-        #     more = next_more
+        
     task = taskqueue.add(
             queue_name='backend',
             url='/waypoint_updates',
@@ -83,7 +101,6 @@ def execute_waypoint_update(flight_waypoints_key, flight_key):
     flight_waypoints.version = flight.version
     yield flight_waypoints.put_async()
     response = {"Waypoint": [flight_waypoints.next_waypoint.lat, flight_waypoints.next_waypoint.lon], 
-        "Dest Waypoint": [flight_waypoints.dest_waypoint.lat, flight_waypoints.dest_waypoint.lon], 
         "Speed": flight_waypoints.next_speed, 
         "Altitude": flight_waypoints.next_altitude}
     raise ndb.Return(response)
